cd4baab7cc2fa7cbaf05a1e98fc1792f740a6843
aulya rea sagita <aulyarea17@gmail.com>
2025-03-12 15:32:26 +0700

    menambahkan folder pemweb-2

---
+++ webphp2025-pemweb2/welcome.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Selamat belajar Web </h3>
+    <?php
+    echo "<p>Skrip PHP Pertama Saya</p>";
+    ?>
+
+    <?php
+
+        $_nama ="Aulya rea";
+        $_umur ="18";
+        $_berat ="45 kg";
+        $_prodi ="Sistem Informasi";
+        $_kampus ="STT-NF";
+    ?>
+
+    <hr>
+
+    <?php
+        echo "Nama saya $_nama. Saya berumur $_umur";
+        echo "<br>Berat badan saya $_berat";
+        echo "<br>dengan prodi saya $_prodi";
+
+    ?>
+    
+</body>
+</html>
